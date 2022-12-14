c75c518e433644141cae5604a024c54029661c35
Gareth Bowen <gareth@bowenwebdesign.co.nz>
2022-12-14 16:44:25 +1300

    pass in arguments

--- src/bar.js
+++ src/bar.js
@@ -1,6 +1,19 @@
+const getValue = function(obj) {
+  let val;
+  if (obj.t === 'arr') {
+    val = obj.v && obj.v.length && obj.v[0];
+  } else {
+    val = obj.v;
+  }
+  if (!val) {
+    return 0;
+  }
+  const parsed = parseInt(val.textContent);
+  return isNaN(parsed) ? 0 : parsed;
+};
+
 export default function bar(first, second) {
-  console.log(first, second);
-  const average = (first.v + second.v) / 2;
+  const average = (getValue(first) + getValue(second)) / 2;
   return {
     t: 'num',
     v: average
